0107612eb2a12c6cf55ad5fd4504a0b8fc9e239b
0x44 <83523587+CPScript@users.noreply.github.com>
2025-07-26 03:53:21 -0400

    Update syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,89 +1,61 @@
-#include "syscall.h"
-#include "interrupts/idt.h"
+#include <stdint.h>
 #include "kernel.h"
+#include "interrupts/idt.h"
 
-static syscall_handler_t syscall_table[256];
+// System call numbers
+#define SYS_EXIT    1
+#define SYS_FORK    2
+#define SYS_READ    3
+#define SYS_WRITE   4
+#define SYS_OPEN    5
+#define SYS_CLOSE   6
+#define SYS_WAITPID 7
+#define SYS_CREAT   8
+#define SYS_LINK    9
+#define SYS_UNLINK  10
+
+// System call handler
+void syscall_handler(uint32_t eax, uint32_t ebx, uint32_t ecx, uint32_t edx) {
+    switch (eax) {
+        case SYS_EXIT:
+            // Handle exit system call
+            printf("Process exiting with code: %d\n", ebx);
+            break;
+            
+        case SYS_FORK:
+            // Handle fork system call
+            printf("Fork system call not implemented\n");
+            break;
+            
+        case SYS_READ:
+            // Handle read system call
+            printf("Read system call: fd=%d, buf=0x%x, count=%d\n", ebx, ecx, edx);
+            break;
+            
+        case SYS_WRITE:
+            // Handle write system call
+            if (ebx == 1) { // stdout
+                char *str = (char*)ecx;
+                for (uint32_t i = 0; i < edx; i++) {
+                    if (str[i] == '\0') break;
+                    putchar(str[i]);
+                }
+            }
+            break;
+            
+        default:
+            printf("Unknown system call: %d\n", eax);
+            break;
+    }
+}
+
+// Assembly syscall handler wrapper
+extern void syscall_handler_asm(void);
 
 void syscall_init(void) {
-    // Set up system call interrupt (interrupt 0x80)
-    idt_set_gate(0x80, (uint32_t)syscall_handler_asm, 0x08, IDT_FLAG_PRESENT | IDT_GATE_INT32 | IDT_FLAG_RING3);
-    
-    // Initialize system call table
-    syscall_table[SYS_EXIT] = (syscall_handler_t)sys_exit;
-    syscall_table[SYS_FORK] = (syscall_handler_t)sys_fork;
-    syscall_table[SYS_READ] = (syscall_handler_t)sys_read;
-    syscall_table[SYS_WRITE] = (syscall_handler_t)sys_write;
-    syscall_table[SYS_OPEN] = (syscall_handler_t)sys_open;
-    syscall_table[SYS_CLOSE] = (syscall_handler_t)sys_close;
-    syscall_table[SYS_GETPID] = (syscall_handler_t)sys_getpid;
-    syscall_table[SYS_BRK] = (syscall_handler_t)sys_brk;
-}
-
-uint32_t syscall_dispatch(uint32_t call_num, uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4, uint32_t arg5) {
-    if (call_num >= 256 || !syscall_table[call_num]) {
-        return -1; // Invalid system call
-    }
-    
-    return syscall_table[call_num](arg1, arg2, arg3, arg4, arg5);
-}
-
-// System call implementations
-uint32_t sys_exit(uint32_t status) {
-    // Exit current process
-    print_message("Process exiting\n");
-    return 0;
-}
-
-uint32_t sys_fork(void) {
-    // Fork current process
-    print_message("Fork not implemented\n");
-    return -1;
-}
-
-uint32_t sys_read(uint32_t fd, uint32_t buffer, uint32_t count) {
-    // Read from file descriptor
-    (void)fd;
-    (void)buffer;
-    (void)count;
-    return 0;
-}
-
-uint32_t sys_write(uint32_t fd, uint32_t buffer, uint32_t count) {
-    // Write to file descriptor
-    if (fd == 1 || fd == 2) { // stdout or stderr
-        char *str = (char*)buffer;
-        for (uint32_t i = 0; i < count; i++) {
-            if (str[i] == 0) break;
-            putchar(str[i]);
-        }
-        return count;
-    }
-    return -1;
-}
-
-uint32_t sys_open(uint32_t pathname, uint32_t flags, uint32_t mode) {
-    // Open file
-    (void)pathname;
-    (void)flags;
-    (void)mode;
-    return -1;
-}
-
-uint32_t sys_close(uint32_t fd) {
-    // Close file descriptor
-    (void)fd;
-    return 0;
-}
-
-uint32_t sys_getpid(void) {
-    // Get process ID
-    return 1; // Stub
-}
-
-uint32_t sys_brk(uint32_t addr) {
-    // Set program break
-    (void)addr;
-    return 0;
+    // Register system call interrupt (usually INT 0x80)
+    idt_set_gate(0x80, (uint32_t)syscall_handler_asm, 0x08, 
+                 IDT_FLAG_PRESENT | IDT_FLAG_RING3 | IDT_GATE_INT32);
 }
 
 // Assembly wrapper for system call handler
@@ -91,32 +63,30 @@ asm(
     ".global syscall_handler_asm\n"
     "syscall_handler_asm:\n"
     "    cli\n"
-    "    push %eax\n"      // Save registers
-    "    push %ecx\n"
-    "    push %edx\n"
-    "    push %ebx\n"
-    "    push %esp\n"
-    "    push %ebp\n"
-    "    push %esi\n"
-    "    push %edi\n"
+    "    pusha\n"
+    "    push %ds\n"
+    "    push %es\n"
+    "    push %fs\n"
+    "    push %gs\n"
     "    \n"
-    "    push %edi\n"      // arg5
-    "    push %esi\n"      // arg4  
-    "    push %edx\n"      // arg3
-    "    push %ecx\n"      // arg2
-    "    push %ebx\n"      // arg1
-    "    push %eax\n"      // syscall number
-    "    call syscall_dispatch\n"
-    "    add $24, %esp\n"  // Clean up arguments
+    "    mov $0x10, %ax\n"  // Load kernel data segment
+    "    mov %ax, %ds\n"
+    "    mov %ax, %es\n"
+    "    mov %ax, %fs\n"
+    "    mov %ax, %gs\n"
     "    \n"
-    "    pop %edi\n"       // Restore registers
-    "    pop %esi\n"
-    "    pop %ebp\n"
-    "    add $4, %esp\n"   // Skip ESP
-    "    pop %ebx\n"
-    "    pop %edx\n"
-    "    pop %ecx\n"
-    "    add $4, %esp\n"   // Skip EAX (return value)
+    "    push %edx\n"       // 4th parameter
+    "    push %ecx\n"       // 3rd parameter
+    "    push %ebx\n"       // 2nd parameter
+    "    push %eax\n"       // 1st parameter (syscall number)
+    "    call syscall_handler\n"
+    "    add $16, %esp\n"   // Clean up parameters
+    "    \n"
+    "    pop %gs\n"
+    "    pop %fs\n"
+    "    pop %es\n"
+    "    pop %ds\n"
+    "    popa\n"
     "    sti\n"
     "    iret\n"
 );
