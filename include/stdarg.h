5e32445204863a21632b92354cd621a381d3b038
0x44 <83523587+CPScript@users.noreply.github.com>
2025-07-26 03:54:34 -0400

    Update stdarg.h

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,12 +1,13 @@
 #ifndef STDARG_H
 #define STDARG_H
 
-// Use GCC built-in variadic argument support
+/* Variable argument list type */
 typedef __builtin_va_list va_list;
 
-#define va_start(ap, last) __builtin_va_start(ap, last)
-#define va_arg(ap, type)   __builtin_va_arg(ap, type)
-#define va_end(ap)         __builtin_va_end(ap)
-#define va_copy(dest, src) __builtin_va_copy(dest, src)
+/* Macros for variable argument handling */
+#define va_start(v, l) __builtin_va_start(v, l)
+#define va_end(v)      __builtin_va_end(v)
+#define va_arg(v, l)   __builtin_va_arg(v, l)
+#define va_copy(d, s)  __builtin_va_copy(d, s)
 
 #endif
