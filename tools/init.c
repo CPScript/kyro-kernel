7785a76078aa57e5f4ae20d35dcbcc7552aafd30
0x44 <83523587+CPScript@users.noreply.github.com>
2025-07-27 01:25:14 -0400

    Update init.c

--- tools/init.c
+++ tools/init.c
@@ -1,20 +1,16 @@
-#include "../kernel/kernel.h"
-#include "../kernel/user.h"  
-#include "../include/fs/fs.h"
-#include "../kernel/printf.h"
-
-void init_users();
-void init_file_system();
+#include "kernel.h"
+#include "user.h"  
+#include "fs/fs.h"
 
 void init_users(void) {
     add_user("admin", "password1", true); 
-    add_user("user1", "userpass1", false); /
+    add_user("user1", "userpass1", false);
     printf("User system initialized with %d users.\n", user_count);
 }
 
 void init_file_system(void) {
     create_directory("home");
     create_file("readme.txt", false);
-    write_file("readme.txt", "Welcome to the kyro OS!\nHours spent making this: 377");
+    write_file("readme.txt", "Welcome to the kyro OS!\nHours spent making this: 379");
     printf("File system initialized with %d files.\n", file_count);
 }
