652baf73372ae4c15cf42a96efe3c1fa1660a735
0x44 <83523587+CPScript@users.noreply.github.com>
2025-07-26 23:16:59 -0400

    Update init.c

--- tools/init.c
+++ tools/init.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "user.h"
-#include "fs.h"
+#include "kernel.h"
+#include "user.h"  
+#include "fs/fs.h"
 
-// Function prototypes
 void init_users();
 void init_file_system();
 
 void init_users() {
-    add_user("admin", "adminpass", true); // Example of an admin user
-    add_user("user1", "user1pass", false); // Example of a regular user
+    add_user("admin", "password1", true); 
+    add_user("user1", "userpass1", false); /
     printf("User system initialized with %d users.\n", user_count);
 }
 
 void init_file_system() {
-    // Optionally, you could create some default files or directories
     create_directory("home");
-    create_file("readme.txt");
-    strcpy(file_list[file_count - 1].content, "Welcome to the kyro OS!\nenjoy your stay.");
+    create_file("readme.txt", false);
+    write_file("readme.txt", "Welcome to the kyro OS!\Hours spent making this; 377.");
     printf("File system initialized with %d files.\n", file_count);
-}
-
-void kernel_init() {
-    init_users();
-    init_file_system();
-    printf("Initialization complete. You can now boot kyro OS.\n");
 }
