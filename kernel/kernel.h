d603a52a02b27a5a7122742c7856bc2e8f10f2a1
0x44 <83523587+CPScript@users.noreply.github.com>
2025-07-26 22:52:40 -0400

    Update kernel.h

--- kernel/kernel.h
+++ kernel/kernel.h
@@ -3,9 +3,9 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdarg.h>
 
-// Forward declarations for size_t
-typedef unsigned int size_t;
+typedef uint32_t size_t;
 
 // Memory management functions
 uint32_t kmalloc(uint32_t size);
@@ -21,53 +21,49 @@ void *memcpy(void *dest, const void *src, size_t n);
 // String functions
 size_t strlen(const char *s);
 char *strcpy(char *dest, const char *src);
-char *strcat(char *dest, const char *src);
-int strcmp(const char *s1, const char *s2);
-int strncmp(const char *s1, const char *s2, size_t n);
 char *strncpy(char *dest, const char *src, size_t n);
+int strcmp(const char *s1, const char *s2);
 
 // Basic I/O
 void print_message(const char *message);
 void clear_screen(void);
+void putchar(char c);
+void puts(const char *str);
 
 // Standard library functions
 int printf(const char *format, ...);
 int scanf(const char *format, ...);
-int sprintf(char *str, const char *format, ...);
-int sscanf(const char *str, const char *format, ...);
-int vsprintf(char *str, const char *format, __builtin_va_list ap);
-int vsscanf(const char *str, const char *format, __builtin_va_list ap);
-
-// Character I/O
-void putchar(char c);
-void puts(const char *str);
+int vsprintf(char *str, const char *format, va_list args);
+int vsscanf(const char *str, const char *format, va_list args);
 
 // Initialization functions
 void init_users(void);
 void init_file_system(void);
-
-// System functions
 void fs_init(void);
 void terminal_init(void);
 void user_init(void);
 void shell_init(void);
 void package_manager_init(void);
 void scripting_init(void);
-
-// Process management
-void create_process(const char *name, void (*entry_point)(void), bool kernel_mode);
-void schedule(void);
-
-// Page directory creation (forward declaration)
-struct page_directory;
-struct page_directory *create_page_directory(void);
-void alloc_frame(uint32_t virtual_addr, bool is_kernel, bool is_writable);
+void syscall_init(void);
+void process_init(void);
 
 // Core kernel functions
 void kernel_init(void);
 void kernel_main(void);
 
-// Global variables
-extern uint32_t placement_address;
+// Process management
+void schedule(void);
+process_t *create_process(const char *name, void (*entry_point)(void), bool kernel_mode);
+
+// Forward declaration for process_t
+typedef struct process process_t;
+
+// Paging functions
+page_directory_t *create_page_directory(void);
+void alloc_frame(uint32_t virtual_addr, bool is_kernel, bool is_writable);
+
+// Forward declaration for page_directory_t
+typedef struct page_directory page_directory_t;
 
 #endif
