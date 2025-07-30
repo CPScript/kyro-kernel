73eb079d3cd33036f358b6059b47df1f56d86aa8
0x44 <83523587+CPScript@users.noreply.github.com>
2025-07-29 23:42:41 -0400

    Add files via upload

---
+++ include/drivers/network.h
@@ -0,0 +1,10 @@
+#ifndef DRIVERS_NETWORK_H
+#define DRIVERS_NETWORK_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+// Function prototypes
+void network_init(void);
+
+#endif // DRIVERS_NETWORK_H
---
+++ include/stddef.h
@@ -0,0 +1,32 @@
+#ifndef STDDEF_H
+#define STDDEF_H
+
+/* Standard definitions */
+
+/* NULL pointer constant */
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+/* Size type */
+#ifndef _SIZE_T_DEFINED
+#define _SIZE_T_DEFINED
+typedef unsigned int size_t;
+#endif
+
+/* Pointer difference type */
+#ifndef _PTRDIFF_T_DEFINED
+#define _PTRDIFF_T_DEFINED
+typedef int ptrdiff_t;
+#endif
+
+/* Wide character type */
+#ifndef _WCHAR_T_DEFINED
+#define _WCHAR_T_DEFINED
+typedef unsigned short wchar_t;
+#endif
+
+/* Offset of member in structure */
+#define offsetof(type, member) ((size_t)&((type*)0)->member)
+
+#endif /* STDDEF_H */
