eb540ad5a6e6f661555620be18f86f6fb98a7f2f
0x44 <83523587+CPScript@users.noreply.github.com>
2025-07-30 18:04:23 -0400

    Delete kernel directory

--- kernel/drivers/mouse.c
+++
@@ -1,8 +0,0 @@
-#include "drivers/mouse.h"
-#include "kernel.h"
-
-void mouse_init(void) {
-    print_message("Mouse driver initialized (stub)\n");
-    // Mouse initialization would go here
-    // For now, just a placeholder since its not needed
-}
